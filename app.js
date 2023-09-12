cca3434ebef368d0db6e998b9f567d05054d9494
sol2588 <dsk2588@gmail.com>
2023-09-12 22:13:27 +0900

    server running check

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const express = require("express");
-const app = express();
 
-app.listen(4001);
+const app = express();
+app.get("/", (req, res) => {
+  res.send("Home");
+  console.log("done");
+});
+
+app.listen(4000, () => {
+  console.log("Server is running on 3000");
+});
