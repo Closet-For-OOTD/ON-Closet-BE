15309227e345041dc29aca685a4938caa253e96a
sol2588 <dsk2588@gmail.com>
2023-09-13 06:27:21 +0900

    multer setting

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 const express = require("express");
+const multer = require("multer");
+const bodyParser = require("body-parser");
+
 const app = express();
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, done) {
+      done(null, "public/files");
+    },
+    filename: function (req, file, done) {
+      const filename = file.originalname;
+      done(null, filename);
+    },
+  }),
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   console.log("연결");
